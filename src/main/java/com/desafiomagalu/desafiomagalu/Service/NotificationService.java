86dcf3cd5e33791572cb3d61321e44c53a5734b0
Felipe Seiji <fenishina@gmail.com>
2024-11-27 21:27:24 -0300

    Criação do getNotification

--- src/main/java/com/desafiomagalu/desafiomagalu/Service/NotificationService.java
+++ src/main/java/com/desafiomagalu/desafiomagalu/Service/NotificationService.java
@@ -1,17 +1,27 @@
-package com.desafiomagalu.desafiomagalu.Service;
+package com.desafiomagalu.desafiomagalu.service;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.desafiomagalu.desafiomagalu.DTO.NotificationDTO;
+import com.desafiomagalu.desafiomagalu.domain.notification.Notification;
 import com.desafiomagalu.desafiomagalu.repository.NotificationRepository;
+
 
 @Service
 public class NotificationService {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    @Transactional
     public void sendNotifications(NotificationDTO dto){
         notificationRepository.save(dto.toNotification());
+    }
+
+    public Optional<Notification> findById(Long notificationId){
+        return notificationRepository.findById(notificationId);
     }
 }
--- src/main/java/com/desafiomagalu/desafiomagalu/controller/NotificationController.java
+++ src/main/java/com/desafiomagalu/desafiomagalu/controller/NotificationController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafiomagalu.desafiomagalu.DTO.NotificationDTO;
-import com.desafiomagalu.desafiomagalu.Service.NotificationService;
+import com.desafiomagalu.desafiomagalu.domain.notification.Notification;
+import com.desafiomagalu.desafiomagalu.service.NotificationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/notification")
 public class NotificationController {
+    
     @Autowired
     private NotificationService notificationService;
     
@@ -22,6 +26,17 @@ public class NotificationController {
     public ResponseEntity<Void> sendNotification(@RequestBody NotificationDTO dto) {
         notificationService.sendNotifications(dto);
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId){
+        var notification = notificationService.findById(notificationId);
+
+        if (notification.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(notification.get());
     }
     
 }
